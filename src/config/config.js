a1ff5dd068220748c8a2c15b062bbae7fbfdd522
mohan4787 <bishwoshah.it@gmail.com>
2026-02-09 16:25:40 +0545

    smtp server connection

--- src/config/config.js
+++ src/config/config.js
@@ -3,9 +3,20 @@ require("dotenv").config()
 const AppConfig = {
     cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
     cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
-    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
+    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+    frontendUrl: process.env.FRONTEND_URL
+}
+
+const SMTPConfig = {
+    provider: process.env.SMTP_PROVIDER,
+    host: process.env.SMTP_HOST,
+    port: process.env.STMP_PORT,
+    user: process.env.SMTP_USER,
+    password: process.env.SMTP_PASSWORD,
+    from: process.env.SMTP_FROM
 }
 
 module.exports = {
-    AppConfig
+    AppConfig,
+    SMTPConfig
 }
