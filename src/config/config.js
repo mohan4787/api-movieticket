50fb29375e9c51d4d2dee410c3fbf2d6441574be
mohan4787 <bishwoshah.it@gmail.com>
2026-02-25 12:32:46 +0545

    connecting mongodb server

--- src/config/config.js
+++ src/config/config.js
@@ -16,7 +16,13 @@ const SMTPConfig = {
     from: process.env.SMTP_FROM
 }
 
+const DbConfig = {
+  mongoDBUrl: process.env.MONGODB_URL,
+  mongoDBName: process.env.MONGODB_DBNAME,
+}
+
 module.exports = {
     AppConfig,
-    SMTPConfig
+    SMTPConfig,
+    DbConfig
 }
--- src/config/express.config.js
+++ src/config/express.config.js
@@ -1,4 +1,5 @@
 const express = require("express");
+require("./mongodb.config");
 const router = require("./router.config");
 const { deleteFile } = require("../utilities/helper");
 const app = express();
